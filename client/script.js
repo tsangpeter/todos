dc0404b38582a46b77f2e279a7a89a405a559a79
Peter Tsang <ptsang408@gmail.com>
2017-02-01 15:34:49 -0500

    complete

---
+++ client/script.js
@@ -0,0 +1,152 @@
+var $ = require('jquery');
+var todoTemplate = require("../views/partials/todo.hbs");
+
+$(function() {
+   $(":button").on('click', addTodo);
+});
+
+$(":text").on('keypress',function(e) {
+  var key = e.keyCode;
+  if( key == 13 || key == 169) {
+    addTodo();
+    e.preventDefault();
+    e.stopPropagation();
+    return false;
+  }
+ });
+
+var addTodo = function() {
+   var text = $('#add-todo-text').val();
+   $.ajax({
+     url: '/api/todos',
+     type: 'POST',
+     data: {
+       text: text
+     },
+     dataType: 'json',
+     success: function(data) {
+      var todo = data.todo[0];
+      var newLiHtml = todoTemplate(todo);
+      $('form + ul').append(newLiHtml);
+      $('#add-todo-text').val('');
+    }
+   });
+ };
+ var updateTodo = function(id, data, cb) {
+  $.ajax({
+    url: '/api/todos/'+id,
+    type: 'PUT',
+    data: data,
+    dataType: 'json',
+    success: function(data) {
+      cb();
+    }
+  });
+};
+var deleteTodo = function(id, cb) {
+  $.ajax({
+    url: '/api/todos/'+id,
+    type: 'DELETE',
+    data: {
+      id: id
+    },
+    dataType: 'json',
+    success: function(data) {
+      cb();
+    }
+  });
+};
+
+var deleteTodoLi = function($li) {
+  $li.remove();
+};
+
+document.addEventListener("DOMContentLoaded", function(event) {
+	var checkboxes = getElementsByTagName('input');
+	for (var i = 0; i < checkboxes.length; i++) {
+		checkboxes[i].addEventListener('click', 'clickHandler');
+	}
+  initTodoObserver();
+});
+
+$('ul').on('change', 'li :checkbox', function() {
+    var $this = $(this),
+        $input = $this[0],
+        $li = $this.parent(),
+        id = $li.attr('id'),
+        checked = $input.checked,
+        data = { done: checked };
+    updateTodo(id, data, function(d) {
+      $this.next().toggleClass('checked');
+    });
+  });
+$('ul').on('keydown', 'li span', function(e) {
+ var $this = $(this),
+     $span = $this[0],
+     $li = $this.parent(),
+     id = $li.attr('id'),
+     key = e.keyCode,
+     target = e.target,
+     text = $span.innerHTML,
+     data = { text: text};
+ $this.addClass('editing');
+ if(key === 27) { //escape key
+   $this.removeClass('editing');
+   document.execCommand('undo');
+   target.blur();
+ } else if(key === 13) { //enter key
+   updateTodo(id, data, function(d) {
+     $this.removeClass('editing');
+     target.blur();
+   });
+   e.preventDefault();
+ }
+});
+$('ul').on('click', 'li a', function() {
+  var $this = $(this),
+  $input = $this[0],
+  $li = $this.parent(),
+  id = $li.attr('id');
+  deleteTodo(id, function(e){
+              deleteTodoLi($li);
+  });
+});
+var initTodoObserver = function () {
+   var target = $('ul')[0];
+   var config = { attributes: true, childList: true, characterData: true };
+   var observer = new MutationObserver(function(mutationRecords) {
+     $.each(mutationRecords, function(index, mutationRecord) {
+       updateTodoCount();
+     });
+   });
+   if(target) {
+     observer.observe(target, config);
+   }
+   updateTodoCount();
+ };
+ var updateTodoCount = function () {
+   $(".count").text($("li").length);
+ };
+ $('.filter').on('click', '.show-all', function() {
+   $('.hide').removeClass('hide');
+ });
+ $('.filter').on('click', '.show-not-done', function() {
+   $('.hide').removeClass('hide');
+   $('.checked').closest('li').addClass('hide');
+ });
+ $('.filter').on('click', '.show-done', function() {
+   $('li').addClass('hide');
+   $('.checked').closest('li').removeClass('hide');
+ });
+ $(".clear").on("click", function() {
+   var $doneLi = $(".checked").closest("li");
+   for (var i = 0; i < $doneLi.length; i++) {
+     var $li = $($doneLi[i]); //you get a li out, and still need to convert into $li
+     var id = $li.attr('id');
+     (function($li){
+       deleteTodo(id, function(){
+                       deleteTodoLi($li);
+       });
+     })($li);
+   }
+ });
--- models/todos.js
+++ models/todos.js
@@ -1,7 +1,12 @@
-var todos = [
-	'fix mac',
-	'set up video confernace',
-	'do the thing'
-];
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var Promise = require('bluebird');
+Promise.promisifyAll(mongoose);
 
-module.exports = todos;
+var TodoSchema = new Schema({
+	text: {type: 'String', require: true},
+	done: {type: 'Boolean'}
+});
+var Todo = mongoose.model('Todo', TodoSchema);
+
+module.exports = Todo;
